eeb4e4345e5adc49b2ab117e8cfd7c56e6f63c4f
Karl Keller <kgk86@hotmail.com>
2022-10-24 03:05:12 +0200

    Hotwire : Turbo Streams example

--- app/controllers/other_controller.rb
+++ app/controllers/other_controller.rb
@@ -1,2 +1,8 @@
-class OtherController < ApplicationController  
+class OtherController < ApplicationController
+  # Turbo streams example
+  def post_something
+    respond_to do |format|
+      format.turbo_stream {  }
+    end
+  end
 end
